0e62edbc36c2b2d76e998499f0a9f0d0f1338105
[TASANAI <[kevin_cox@cmu.ac.th]>
2017-03-20 18:18:19 +0700

    change year

--- src/main/java/camt/se234/unittest/dao/UserDaoImpl.java
+++ src/main/java/camt/se234/unittest/dao/UserDaoImpl.java
@@ -6,6 +6,8 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import static camt.se234.unittest.entity.User.builder;
+
 public class UserDaoImpl {
     List<User> users;
     public UserDaoImpl() {
@@ -14,9 +16,9 @@ public class UserDaoImpl {
                 LocalDate.of(1979,2,14),"08612345678"));
         users.add(new User("Tucky","5675","Tuckung",
                 LocalDate.of(1999,8,30),"08687654321"));
-        users.add(User.builder().name("Honey").username("Honey").password("aabbcc")
+        users.add(builder().name("Honey").username("Honey").password("aabbcc")
                 .dateOfBirth(LocalDate.of(2012,11,13)).telephoneNo("0000000000").build());
-        users.add(User.builder().name("NoName").username("None").password("none")
+        users.add(builder().name("NoName").username("None").password("none")
                 .dateOfBirth(LocalDate.of(2112,1,1)).telephoneNo("9999999999").build());
     }
 
