5935655694329a36631d2a2a1fae97673983cddb
Siriganya Sensupa <siriganya_sen@cmu.ac.th>
2017-03-27 17:40:34 +0700

    mock pattern

--- src/test/java/camt/se234/unittest/dao/UserDaoImplTest.java
+++ src/test/java/camt/se234/unittest/dao/UserDaoImplTest.java
@@ -24,18 +24,26 @@ import static org.mockito.Mockito.when;
 public class UserDaoImplTest {
     @Test
     public void testGetUsers() {
-        UserDaoImpl userDao = new UserDaoImpl();
-        assertThat(userDao.getUsers(),
-                hasItems(new User("Prayuth", "1234", "Tu",
-                                LocalDate.of(1979, 2, 14), "08612345678"),
-                        new User("Tucky", "5675", "Tuckung",
-                                LocalDate.of(1999, 8, 30), "08687654321"),
-                        new User("Honey", "aabbcc", "Honey",
-                                LocalDate.of(2012, 11, 13), "0000000000"),
-                        new User("None", "none", "NoName",
-                                LocalDate.of(2112, 1, 1), "9999999999")
 
+        UserDaoImpl userDao = mock(UserDaoImpl.class);
+        when(userDao.getUsers())
+                .thenReturn(Arrays.asList(
+                        new User("" , "", "",
+                                LocalDate.of(,,), "")
                 ));
+
+//        UserDaoImpl userDao = new UserDaoImpl();
+//        assertThat(userDao.getUsers(),
+//                hasItems(new User("Prayuth", "1234", "Tu",
+//                                LocalDate.of(1979, 2, 14), "08612345678"),
+//                        new User("Tucky", "5675", "Tuckung",
+//                                LocalDate.of(1999, 8, 30), "08687654321"),
+//                        new User("Honey", "aabbcc", "Honey",
+//                                LocalDate.of(2012, 11, 13), "0000000000"),
+//                        new User("None", "none", "NoName",
+//                                LocalDate.of(2112, 1, 1), "9999999999")
+//
+//                ));
 
         assertThat(userDao.getUsers(),
                 contains(new User("Prayuth", "1234", "Tu",
